dc5dc7f0bd8ef72f9e538c84c2d7d918ac1b9009
Roman Babarika <rostachok@yandex.ru>
2025-10-20 11:37:03 +0300

    with Sorting

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Btucher");   
+﻿using System.Numerics;
+
+class Program
+{
+    static void functionSorting(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = i; j < array.Length; j++)
+            {
+                if (array[i] > array[j])
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+    static void Main()
+    {
+        int[] arr = { 3,7,5};
+        functionSorting(arr);
+        for (int i =0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
+
